1edfe56a3faa4d92ed09f6021ffeabe5217396e2
ParkMinWoo98 <mypmw13579@gmail.com>
2025-10-20 15:16:43 +0900

    project 생성 lyra resouce 추가

---
+++ Source/MegaBonkLike/MegaBonkLike.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MegaBonkLike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MegaBonkLike, "MegaBonkLike" );
