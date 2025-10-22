5e814634d296f795d9e264f6ef598c9fe7302470
ParkMinWoo98 <mypmw13579@gmail.com>
2025-10-22 23:16:01 +0900

    플레이어 애니메이션 추가

---
+++ Source/MegaBonkLike/Animation/MBLAnimInstance.cpp
@@ -0,0 +1,27 @@
+﻿#include "Animation/MBLAnimInstance.h"
+#include "Entity/MBLCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UMBLAnimInstance::NativeInitializeAnimation()
+{
+	APawn* OwnerPawn = TryGetPawnOwner();
+	if (IsValid(OwnerPawn) == true)
+	{
+		OwnerCharacter = Cast<AMBLCharacter>(OwnerPawn);
+		OwnerCharacterMovement = OwnerCharacter->GetCharacterMovement();
+	}
+}
+
+void UMBLAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	if (IsValid(OwnerCharacter) == true && IsValid(OwnerCharacterMovement) == true)
+	{
+		Velocity = OwnerCharacterMovement->Velocity;
+		GroundSpeed = Velocity.Size2D();
+
+		float GroundAcceleration = OwnerCharacterMovement->GetCurrentAcceleration().Size2D();
+		bool bIsAccelerationNearlyZero = FMath::IsNearlyZero(GroundAcceleration);
+		bShouldMove = (KINDA_SMALL_NUMBER < GroundSpeed) && (bIsAccelerationNearlyZero == false);
+		bIsFalling = OwnerCharacterMovement->IsFalling();
+	}
+}
---
+++ Source/MegaBonkLike/Animation/MBLAnimInstance.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MBLAnimInstance.generated.h"
+
+class AMBLCharacter;
+class UCharacterMovementComponent;
+
+UCLASS()
+class MEGABONKLIKE_API UMBLAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<AMBLCharacter> OwnerCharacter;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UCharacterMovementComponent> OwnerCharacterMovement;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FVector Velocity;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float GroundSpeed;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	uint8 bShouldMove : 1;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	uint8 bIsFalling : 1;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	uint8 bIsDead : 1;
+};
