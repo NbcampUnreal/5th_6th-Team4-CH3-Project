8a8f177b0132bd2ec75aea5aec3523c143d8109e
Kwon IK Hwan <ksi9421@gmail.com>
2025-10-23 15:39:43 +0900

    NPC/Gimmick 1차병합 및 파일통합

--- Source/MegaBonkLike/Game/MBLGameModeBase.cpp
+++
@@ -1,9 +0,0 @@
-#include "Game/MBLGameModeBase.h"
-#include "Controller/MBLPlayerController.h"
-#include "Character/MBLPlayerCharacter.h"
-
-AMBLGameModeBase::AMBLGameModeBase()
-{
-	PlayerControllerClass = AMBLPlayerController::StaticClass();
-	DefaultPawnClass = AMBLPlayerCharacter::StaticClass();
-}
