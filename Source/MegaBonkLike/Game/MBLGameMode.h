87509ae2f6524ed938074c2dc14d816cf94c7981
RPG12165607 <sau03095@naver.com>
2025-10-30 15:46:36 +0900

    MBLPlayerController: ShowEndGameScreen, RestartGame 함수 추가, MBLGameMode: GameOver 함수 추가

--- Source/MegaBonkLike/Game/MBLGameMode.h
+++ Source/MegaBonkLike/Game/MBLGameMode.h
@@ -1,17 +1,20 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameMode.h"
 #include "MBLGameMode.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class MEGABONKLIKE_API AMBLGameMode : public AGameMode
 {
 	GENERATED_BODY()
-	
+
+
+public:
+	virtual void BeginPlay() override;
+
+	FTimerHandle GameOverTimerHandle;
+
+	void GameOver();
+
 };
