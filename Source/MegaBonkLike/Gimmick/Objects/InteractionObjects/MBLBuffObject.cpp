38588e2bb4a87f2ee6003d2e31b0f77946dc7369
SangBeenKim <sb.kim03699@gmail.com>
2025-11-08 11:23:00 +0900

    BurrObject 수정, ShrineOption 추가

--- Source/MegaBonkLike/Gimmick/Objects/InteractionObjects/MBLBuffObject.cpp
+++ Source/MegaBonkLike/Gimmick/Objects/InteractionObjects/MBLBuffObject.cpp
@@ -1,4 +1,6 @@
 #include "Gimmick/Objects/InteractionObjects/MBLBuffObject.h"
+#include "Item/ItemRarityDataRow.h"
+#include "Gimmick/Data/ShrineOption.h"
 
 AMBLBuffObject::AMBLBuffObject()
 {
@@ -8,18 +10,79 @@ void AMBLBuffObject::OnObjectActivated(AActor* Activator)
 {
 	if (SantuaryOptions.IsEmpty()) return;
 
+	const TArray<FShrineOption> SelectedOptions = GetRandomOptions();
+
+	if (!SelectedOptions.IsEmpty())
+	{
+		//UI에 넘기기
+		Super::DestroyObject();
+	}
+}
+
+TArray<FGameplayTag> AMBLBuffObject::GetRandomTag() const
+{
 	TArray<FGameplayTag> OptionTags;
 	SantuaryOptions.GetKeys(OptionTags);
 
-	TArray<FGameplayTag> OptionsToShow;
-	int Count = FMath::Min(3, OptionTags.Num());
+	if (OptionTags.Num() < MinTag) return {};
 
-	for (int i = 0; i < Count; ++i)
+	TArray<FGameplayTag> OptionsToShow;
+
+	for (int i = 0; i < OptionTags.Num(); ++i)
 	{
 		int32 RandomIdx = FMath::RandRange(0, OptionTags.Num() - 1);
 		OptionsToShow.Add(OptionTags[RandomIdx]);
 		OptionTags.RemoveAt(RandomIdx);
 	}
 
-	TMap<FGameplayTag, FAttributeModifier> SelectedOptions;
+	return OptionsToShow;
+}
+
+TArray<FShrineOption> AMBLBuffObject::GetRandomOptions()
+{
+	TArray<FShrineOption> SelectedOptions;
+	TArray<FGameplayTag> OptionsToShow = GetRandomTag();
+
+	if (OptionsToShow.IsEmpty()) return {};
+
+	for (const FGameplayTag& Tag : OptionsToShow)
+	{
+		if (FAttributeModifier* Modifier = SantuaryOptions.Find(Tag))
+		{
+			const FItemRarityDataRow* RandomRarity = GetRandomRarity();
+			if (!RandomRarity) return {};
+			FShrineOption NewOption;
+			NewOption.Rarity = RandomRarity->ItemRarity;
+			NewOption.Tag = Tag;
+			NewOption.Modifier = *Modifier;
+			NewOption.Modifier.Value *= RandomRarity->Multiplier;
+			SelectedOptions.Add(NewOption);
+		}
+	}
+	return SelectedOptions;
+}
+
+const FItemRarityDataRow* AMBLBuffObject::GetRandomRarity() const
+{
+	const auto& RarityRows = ItemRarityTable->GetRowMap();
+	TArray<FItemRarityDataRow*> RarityArray;
+	RarityArray.Reserve(RarityRows.Num());
+	float TotalRarityAppearRate = 0.0f;
+	for (const auto& Row : RarityRows)
+	{
+		FItemRarityDataRow* Data = (FItemRarityDataRow*)Row.Value;
+		TotalRarityAppearRate += Data->AppearanceRate;
+		RarityArray.Add(Data);
+	}
+
+	float RandomRate = FMath::RandRange(0.0f, TotalRarityAppearRate);
+	for (auto* Rarity : RarityArray)
+	{
+		RandomRate -= Rarity->AppearanceRate;
+		if (RandomRate <= 0.0f)
+		{
+			return Rarity;
+		}
+	}
+	return RarityArray.IsEmpty() ? nullptr : *RarityArray.begin();
 }
